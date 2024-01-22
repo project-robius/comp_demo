eb7e2674e60791e6842eb490e7e3cf2b783f239a
Jorge Bejar <jorge@wyeworks.com>
2024-01-22 16:02:29 -0300

    Improve blur effect with multilayered approach

--- src/component_blur.rs
+++ src/component_blur.rs
@@ -4,6 +4,7 @@ use makepad_widgets::*;
 live_design! {
     import makepad_widgets::base::*;
     import makepad_widgets::theme_desktop_dark::*;
+    import makepad_draw::shader::std::*;
 
     FONT_LABEL = {
         font_size: 10.0,
@@ -12,93 +13,136 @@ live_design! {
         }
     }
 
-    IMG_0 = dep("crate://self/resources/images/0.png")
-    IMG_1 = dep("crate://self/resources/images/1.png")
-    IMG_2 = dep("crate://self/resources/images/3.png")
-    IMG_3 = dep("crate://self/resources/images/4.png")
+    BlurStage = <ViewBase> {
+        optimize: Texture,
+        draw_bg: {
+            texture image: texture2d
+
+            uniform blursize: 0.0,
+            uniform blurstd: 1.5,
+            uniform blurx: 1.0,
+            uniform blury: 0.0,
+            varying g1: float,
+            varying g2: float,
+            varying g3: float,
+            varying g4: float,
+            varying g5: float,
+
+            varying gaussscale: float,
+
+            varying o0: vec2,
+
+            varying o1a: vec2,
+            varying o2a: vec2,
+            varying o3a: vec2,
+            varying o4a: vec2,
+            varying o5a: vec2,
+
+            varying o1b: vec2,
+            varying o2b: vec2,
+            varying o3b: vec2,
+            varying o4b: vec2,
+            varying o5b: vec2,
+
+            fn vertex(self) -> vec4
+            {
+                let x = self.blurx;
+                let y = self.blury;
+                let dpi = self.dpi_factor;
+                let ceil_size = ceil(self.rect_size * dpi) / dpi
+                let floor_pos = floor(self.rect_pos * dpi) / dpi
+
+                let offset = 0.003 * self.blursize / max(x,y);
+                let standard_deviation = 0.0001 + self.blurstd *0.003;
+                let st_dev_sqr = standard_deviation * standard_deviation;
+
+                let off1 = offset;
+                let off2 = 2.0*offset;
+                let off3 = 3.0*offset;
+                let off4 = 4.0*offset;
+                let off5 = 5.0*offset;
+
+                let mainscale = (1.0 / sqrt(2*PI*st_dev_sqr));
+                let stddevscale = 1.0/ (2*st_dev_sqr);
+
+                self.g1 =  pow(E, -((off1*off1)* stddevscale));
+                self.g2 =  pow(E, -((off2*off2)* stddevscale));
+                self.g3 =  pow(E, -((off3*off3)* stddevscale));
+                self.g4 =  pow(E, -((off4*off4)* stddevscale));
+                self.g5 =  pow(E, -((off5*off5)* stddevscale));
+
+                self.gaussscale = 1.0/(1.0 +  (self.g1 + self.g2 + self.g3 + self.g4 + self.g5 )*2.0);
+
+                let pos = self.clip_and_transform_vertex(self.rect_pos, self.rect_size);
+                self.o0 = self.pos*0.5;
+
+                self.o1a = self.o0 + vec2(off1*x,off1*y);
+                self.o2a = self.o0 + vec2(off2*x,off2*y);
+                self.o3a = self.o0 + vec2(off3*x,off3*y);
+                self.o4a = self.o0 + vec2(off4*x,off4*y);
+                self.o5a = self.o0 + vec2(off5*x,off5*y);
+
+                self.o1b = self.o0 - vec2(off1*x,off1*y);
+                self.o2b = self.o0 - vec2(off2*x,off2*y);
+                self.o3b = self.o0 - vec2(off3*x,off3*y);
+                self.o4b = self.o0 - vec2(off4*x,off4*y);
+                self.o5b = self.o0 - vec2(off5*x,off5*y);
+
+                return pos;
+            }
+
+            fn pixel(self) -> vec4
+            {
+
+                let col = sample2d_rt(self.image, self.o0) ;
+                col +=  (sample2d_rt(self.image, self.o1a) + sample2d_rt(self.image, self.o1b)) * self.g1;
+                col +=  (sample2d_rt(self.image, self.o2a) + sample2d_rt(self.image, self.o2b)) * self.g2 ;
+                col +=  (sample2d_rt(self.image, self.o3a) + sample2d_rt(self.image, self.o3b)) * self.g3 ;
+                col +=  (sample2d_rt(self.image, self.o4a) + sample2d_rt(self.image, self.o4b)) * self.g4 ;
+                col +=  (sample2d_rt(self.image, self.o5a) + sample2d_rt(self.image, self.o5b)) * self.g5 ;
+                col = col * self.gaussscale;
+
+                return col ;
+            }
+        }
+    }
 
     BlurBitmapTextItem = {{BlurBitmapTextItem}} {
         width: Fill,
         height: 400
 
-        element = <CachedView> {
-            width: 300,
-            height: 300,
+        step4 = <BlurStage>{
+            width: Fill,
+            height: Fill,
+            draw_bg:{blury: 0.0, blurx: 10.0}
+            step3 = <BlurStage>{
+                width: Fill,
+                height: Fill,
+                draw_bg:{blury: 10.0, blurx: 0.0}
+                step2 = <BlurStage>{
+                    width: Fill,
+                    height: Fill,
+                    draw_bg:{blury: 7.07, blurx: 7.07}
+                    step1 = <BlurStage>{
+                        width: 300,
+                        height: 300,
 
-            padding: 40.0,
+                        padding: 40.0,
+                        draw_bg:{blury: -7.07, blurx: 7.07}
 
-            draw_bg: {
-                texture image: texture2d
-                instance blur_radius: 10.0
-
-                varying radius: float
-                varying initial_offset: vec2
-                varying step_delta: vec2
-
-                fn get_color_pos(self, pos: vec2) -> vec4 {
-                    return sample2d_rt(self.image, pos);
-                }
-
-                fn coeffs(i: float) -> float {
-                    if i - 0.1 < 0.0 { return 0.0369543295325685 }
-                    else if i - 0.1 < 1.0 { return 0.05707225834646017 }
-                    else if i - 0.1 < 2.0 { return 0.08056214824901288 }
-                    else if i - 0.1 < 3.0 { return 0.10394046353380644 }
-                    else if i - 0.1 < 4.0 { return 0.1279319631769404 }
-                    else if i - 0.1 < 5.0 { return 0.14953785857076586 }
-                    else if i - 0.1 < 6.0 { return 0.13014234492770832 }
-                    else if i - 0.1 < 7.0 { return 0.11718098483901983 }
-                    else if i - 0.1 < 8.0 { return 0.10394046353380644 }
-                    else if i - 0.1 < 9.0 { return 0.0885838874611158 }
-                    else if i - 0.1 < 10.0 { return 0.07253844158670234 }
-                    else if i - 0.1 < 11.0 { return 0.04987059658718462 }
-                    else { return 0.0; }
-                }
-
-                fn vertex(self) -> vec4 {
-                    self.radius = self.blur_radius / 10000.0 * self.rect_size.x;
-                    self.initial_offset = vec2(-16.0 / self.rect_size.x, -16.0 / self.rect_size.y);
-                    self.step_delta = vec2(3.0 / self.rect_size.x, 3.0 / self.rect_size.y);
-
-                    return self.clip_and_transform_vertex(self.rect_pos, self.rect_size * 2.0)
-                }
-
-                fn pixel(self) -> vec4 {
-                    let color = vec4(0.0, 0.0, 0.0, 0.0);
-                    let pos = self.pos;
-                    let i = 0.0;
-                    let j = 0.0;
-
-                    let posx = self.initial_offset.x;
-                    let posy = self.initial_offset.y;
-                    for step in 0..11 {
-                        j = 0.0;
-                        posy = self.initial_offset.y;
-                        for b in 0..11 {
-                            pos = self.pos + vec2(posx, posy) * self.radius;
-                            color += self.get_color_pos(pos) * coeffs(i) * coeffs(j);
-
-                            posy += self.step_delta.x;
-                            j += 1.0;
+                        <Label> {
+                            text: "长恨歌\n作者：白居易\n汉皇重色思倾国，御宇多年求不得。\n杨家有女初长成，养在深闺人未识。\n天生丽质难自弃，一朝选在君王侧。\n回眸一笑百媚生，六宫粉黛无颜色。\n春寒赐浴华清池，温泉水滑洗凝脂。\n侍儿扶起娇无力，始是新承恩泽时。\n云鬓花颜金步摇，芙蓉帐暖度春宵。..."
+                            draw_text: {
+                                wrap: Word,
+                                text_style: <FONT_LABEL> {
+                                    font_size: 12
+                                }
+            
+                                fn get_color(self) -> vec4 {
+                                    return #FFFFFF
+                                }
+                            }
                         }
-                        posx += self.step_delta.y;
-                        i += 1.0;
-                    }
-                    
-                    return color;
-                }
-            }
-
-            <Label> {
-                text: "长恨歌\n作者：白居易\n汉皇重色思倾国，御宇多年求不得。\n杨家有女初长成，养在深闺人未识。\n天生丽质难自弃，一朝选在君王侧。\n回眸一笑百媚生，六宫粉黛无颜色。\n春寒赐浴华清池，温泉水滑洗凝脂。\n侍儿扶起娇无力，始是新承恩泽时。\n云鬓花颜金步摇，芙蓉帐暖度春宵。..."
-                draw_text: {
-                    wrap: Word,
-                    text_style: <FONT_LABEL> {
-                        font_size: 12
-                    }
-
-                    fn get_color(self) -> vec4 {
-                        return #FFFFFF
                     }
                 }
             }
@@ -110,95 +154,67 @@ live_design! {
                 show = {
                     redraw: true,
                     from: {all: BounceLoop {duration: 2.0, end: 1.0}}
-                    apply: { element = { draw_bg: {blur_radius: 6.0}} }
+                    apply: {
+                        step4 = {
+                            draw_bg: {blursize: 0.8}
+                            step3 = {
+                                draw_bg: {blursize: 0.8}
+                                step2 = {
+                                    draw_bg: {blursize: 0.8}
+                                    step1 = {
+                                        draw_bg: {blursize: 0.8}
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
                 init = {
                     from: {all: Snap}
-                    apply: { element = { draw_bg: {blur_radius: 2.0}} }
+                    apply: {
+                        step4 = {
+                            draw_bg: {blursize: 0.05}
+                            step3 = {
+                                draw_bg: {blursize: 0.05}
+                                step2 = {
+                                    draw_bg: {blursize: 0.05}
+                                    step1 = {
+                                        draw_bg: {blursize: 0.05}
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 
+    IMG_0 = dep("crate://self/resources/images/0.png")
+    IMG_1 = dep("crate://self/resources/images/1.png")
+    IMG_2 = dep("crate://self/resources/images/3.png")
+    IMG_3 = dep("crate://self/resources/images/4.png")
+
+
     BlurImage = {{BlurImage}} {
-        image: <Image> {
+        step4 = <BlurStage>{
             width: 60.0,
             height: 60.0,
-
-            draw_bg: {
-                instance blur_radius: 10.0
-                varying radius: float
-                varying initial_offset: vec2
-                varying step_delta: vec2
-
-                fn get_color_pos(self, pos: vec2) -> vec4 {
-                    return sample2d(self.image, pos).xyzw;
-                }
-
-                fn coeffs(i: float) -> float {
-                    if i - 0.1 < 0.0 { return 0.0369543295325685 }
-                    else if i - 0.1 < 1.0 { return 0.05707225834646017 }
-                    else if i - 0.1 < 2.0 { return 0.08056214824901288 }
-                    else if i - 0.1 < 3.0 { return 0.10394046353380644 }
-                    else if i - 0.1 < 4.0 { return 0.1279319631769404 }
-                    else if i - 0.1 < 5.0 { return 0.14953785857076586 }
-                    else if i - 0.1 < 6.0 { return 0.13014234492770832 }
-                    else if i - 0.1 < 7.0 { return 0.11718098483901983 }
-                    else if i - 0.1 < 8.0 { return 0.10394046353380644 }
-                    else if i - 0.1 < 9.0 { return 0.0885838874611158 }
-                    else if i - 0.1 < 10.0 { return 0.07253844158670234 }
-                    else if i - 0.1 < 11.0 { return 0.04987059658718462 }
-                    else { return 0.0; }
-                }
-
-                fn pixel(self) -> vec4 {
-                    let color = vec4(0.0, 0.0, 0.0, 0.0);
-                    let pos = self.pos;
-                    let i = 0.0;
-                    let j = 0.0;
-
-                    let posx = self.initial_offset.x;
-                    let posy = self.initial_offset.y;
-                    for step in 0..11 {
-                        j = 0.0;
-                        posy = self.initial_offset.y;
-                        for b in 0..11 {
-                            pos = self.pos + vec2(posx, posy) * self.radius;
-                            color += self.get_color_pos(pos) * coeffs(i) * coeffs(j);
-
-                            posy += self.step_delta.x;
-                            j += 1.0;
+            draw_bg:{blury: 0.0, blurx: 10.0}
+            step3 = <BlurStage>{
+                width: Fill,
+                height: Fill,
+                draw_bg:{blury: 10.0, blurx: 0.0}
+                step2 = <BlurStage>{
+                    width: Fill,
+                    height: Fill,
+                    draw_bg:{blury: 7.07, blurx: 7.07}
+                    step1 = <BlurStage> {
+                        image = <Image> {
+                            width: 60.0,
+                            height: 60.0,
                         }
-                        posx += self.step_delta.y;
-                        i += 1.0;
                     }
-                    
-                    return color;
-                }
-
-                fn clip_and_transform_vertex(self, rect_pos:vec2, rect_size:vec2) -> vec4 {
-                    let clipped: vec2 = clamp(
-                        self.geom_pos * rect_size + rect_pos,
-                        self.draw_clip.xy,
-                        self.draw_clip.zw
-                    )
-                    //clipped = self.geom_pos * rect_size + rect_pos;
-                    self.pos = (clipped - rect_pos) / rect_size
-                    // only pass the clipped position forward
-                    return self.camera_projection * (self.camera_view * (self.view_transform * vec4(
-                        clipped.x,
-                        clipped.y,
-                        self.draw_depth + self.draw_zbias,
-                        1.
-                    )))
-                }
-                
-                fn vertex(self) -> vec4 {
-                    self.radius = self.blur_radius / 1000.0 * self.rect_size.x;
-                    self.initial_offset = vec2(-16.0 / self.rect_size.x, -16.0 / self.rect_size.y);
-                    self.step_delta = vec2(3.0 / self.rect_size.x, 3.0 / self.rect_size.y);
-
-                    return self.clip_and_transform_vertex(self.rect_pos, self.rect_size)
                 }
             }
         }
@@ -209,11 +225,37 @@ live_design! {
                 show = {
                     redraw: true,
                     from: {all: BounceLoop {duration: 2.0, end: 1.0}}
-                    apply: { image: { draw_bg: {blur_radius: 8.0}} }
+                    apply: {
+                        step4 = {
+                            draw_bg: {blursize: 5.0}
+                            step3 = {
+                                draw_bg: {blursize: 5.0}
+                                step2 = {
+                                    draw_bg: {blursize: 5.0}
+                                    step1 = {
+                                        draw_bg: {blursize: 5.0}
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
                 init = {
                     from: {all: Snap}
-                    apply: { image: { draw_bg: {blur_radius: 2.0}} }
+                    apply: {
+                        step4 = {
+                            draw_bg: {blursize: 1.0}
+                            step3 = {
+                                draw_bg: {blursize: 1.0}
+                                step2 = {
+                                    draw_bg: {blursize: 1.0}
+                                    step1 = {
+                                        draw_bg: {blursize: 1.0}
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -229,34 +271,34 @@ live_design! {
             width: Fill,
             height: 100.0,
             padding: 20.0,
-            margin: 0.0
+            margin: { left: 25.0 }
 
             <View> {
                 width: Fill,
                 align: {x: 0.5}
                 blur_image_item = <BlurImage> {
-                    image: { source: (IMG_0) }
+                    step4 = { step3 = { step2 = { step1 = { image = { source: (IMG_0) }}}}}
                 }
             }
             <View> {
                 width: Fill,
                 align: {x: 0.5}
                 blur_image_item = <BlurImage> {
-                    image: { source: (IMG_1) }
+                    step4 = { step3 = { step2 = { step1 = { image = { source: (IMG_1) }}}}}
                 }
             }
             <View> {
                 width: Fill,
                 align: {x: 0.5}
                 blur_image_item = <BlurImage> {
-                    image: { source: (IMG_2) }
+                    step4 = { step3 = { step2 = { step1 = { image = { source: (IMG_2) }}}}}
                 }
             }
             <View> {
                 width: Fill,
                 align: {x: 0.5}
                 blur_image_item = <BlurImage> {
-                    image: { source: (IMG_3) }
+                    step4 = { step3 = { step2 = { step1 = { image = { source: (IMG_3) }}}}}
                 }
             }
         }
@@ -304,8 +346,8 @@ live_design! {
 
 #[derive(Live, LiveHook, Widget)]
 pub struct BlurImage {
-    #[live] #[wrap]
-    image: Image,
+    #[deref]
+    view: View,
 
     #[animator]
     animator: Animator,
@@ -314,9 +356,9 @@ pub struct BlurImage {
 impl Widget for BlurImage {
     fn handle_event(&mut self, cx: &mut Cx, event: &Event, scope: &mut Scope) {
         if self.animator_handle_event(cx, event).must_redraw() {
-            self.image.redraw(cx);
+            self.view.redraw(cx);
         }
-        self.image.handle_event(cx, event, scope);
+        self.view.handle_event(cx, event, scope);
     }
 
     fn draw_walk(&mut self, cx: &mut Cx2d, scope: &mut Scope, walk: Walk) -> DrawStep {
@@ -324,7 +366,7 @@ impl Widget for BlurImage {
             self.animator_play(cx, id!(play.show));
         }
 
-        self.image.draw_walk(cx, walk)
+        self.view.draw_walk(cx, scope, walk)
     }
 }
 
