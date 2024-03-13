7ecbbfafde80646691c1cebe3dcd0e437361c3cc
Jorge Bejar <jorge@wyeworks.com>
2024-03-13 15:37:18 -0300

    Implement Component Shadow example with existing blur option in Makepad shader code

--- src/component_shadow.rs
+++ src/component_shadow.rs
@@ -30,35 +30,29 @@ live_design! {
 
         align: {x: 0.5, y: 0.5}
 
-        step4 = <BlurStage>{
-            width: Fill,
-            height: Fill,
-            draw_bg:{blury: 0.0, blurx: 10.0, blursize: (SHADOW_BLUR_SIZE), blurstd: (SHADOW_BLUR_STD)}
-            step3 = <BlurStage>{
-                width: Fill,
-                height: Fill,
-                draw_bg:{blury: 10.0, blurx: 0.0, blursize: (SHADOW_BLUR_SIZE), blurstd: (SHADOW_BLUR_STD)}
-                step2 = <BlurStage>{
-                    width: Fill,
-                    height: Fill,
-                    draw_bg:{blury: 7.07, blurx: 7.07, blursize: (SHADOW_BLUR_SIZE), blurstd: (SHADOW_BLUR_STD)}
-                    step1 = <BlurStage>{
-                        width: Fill,
-                        height: Fill,
+        shadow = <RoundedView> {
+            width: 90,
+            height: 90,
 
-                        draw_bg:{blury: -7.07, blurx: 7.07, blursize: (SHADOW_BLUR_SIZE), blurstd: (SHADOW_BLUR_STD)}
+            draw_bg: {
+                radius: 10.0
+                color: #000
 
-                        align: {x: 0.5, y: 0.5}
-                        shadow = <RoundedView> {
-                            width: 65,
-                            height: 65,
-
-                            draw_bg: {
-                                radius: 10.0
-                                color: #000
-                            }
-                        }
+                fn pixel(self) -> vec4 {
+                    let sdf = Sdf2d::viewport(self.pos * self.rect_size)
+                    sdf.box(
+                        self.inset.x + self.border_width,
+                        self.inset.y + self.border_width,
+                        self.rect_size.x - (self.inset.x + self.inset.z + self.border_width * 2.0),
+                        self.rect_size.y - (self.inset.y + self.inset.w + self.border_width * 2.0),
+                        max(1.0, self.radius)
+                    )
+                    sdf.blur = 20.0;
+                    sdf.fill_keep(self.get_color())
+                    if self.border_width > 0.0 {
+                        sdf.stroke(self.get_border_color(), self.border_width)
                     }
+                    return sdf.result;
                 }
             }
         }
@@ -92,165 +86,20 @@ live_design! {
                     redraw: true,
                     from: {all: BounceLoop {duration: 1.0, end: 1.0}}
                     apply: {
-                        image = { draw_bg: { radius: 15.0 }}
-                        step4 = { step3 = { step2 = { step1 = { shadow = { draw_bg: {radius: 15.0 } }}}}}
+                        image = { draw_bg: { radius: 15.0 } }
+                        shadow = { draw_bg: { radius: 15.0 } }
                     }
                 }
                 init = {
                     from: {all: Snap}
                     apply: {
-                        image = {draw_bg: { radius: 0.0 }}
-                        step4 = { step3 = { step2 = { step1 = { shadow = { draw_bg: {radius: 0.0 } }}}}}
+                        image = { draw_bg: { radius: 3.0 } }
+                        shadow = { draw_bg: { radius: 3.0 } }
                     }
                 }
             }
         }
     }
-
-    // ComponentShadowItem = {{ComponentShadowItem}} {
-    //     width: 70.0,
-    //     height: 70.0
-
-    //     element = <View> {
-    //         width: 64.0,
-    //         height: 64.0,
-    //         align: {x: 0.5, y: 0.5},
-
-    //         show_bg: true,
-    //         draw_bg: {
-    //             instance radius: 10.0
-
-    //             fn gaussian(x: float, sigma: float) -> float {
-    //                 return exp(-(x * x) / (2.0 * sigma * sigma)) / (sqrt(2.0 * PI) * sigma);
-    //             }
-
-    //             fn erf(x: vec2) -> vec2 {
-    //                 let s = sign(x);
-    //                 let a = abs(x);
-    //                 x = 1.0 + (0.278393 + (0.230389 + 0.078108 * (a * a)) * a) * a;
-    //                 x *= x;
-    //                 return s - s / (x * x);
-    //             }
-
-    //             // Return the blurred mask along the x dimension
-    //             fn roundedBoxShadowX(x: float, y: float, sigma: float, corner: float, halfSize: vec2) -> float {
-    //                 let delta = min(halfSize.y - corner - abs(y), 0.0);
-    //                 let curved = halfSize.x - corner + sqrt(max(0.0, corner * corner - delta * delta));
-    //                 let integral = 0.5 + 0.5 * erf((x + vec2(-curved, curved)) * (sqrt(0.5) / sigma));
-    //                 return integral.y - integral.x;
-    //             }
-
-    //             // Return the mask for the shadow of a box from lower to upper
-    //             fn roundedBoxShadow(lower: vec2, upper: vec2, point: vec2, sigma: float, corner: float) -> float {
-    //                 // Center everything to make the math easier
-    //                 let center = (lower + upper) * 0.5;
-    //                 let halfSize = (upper - lower) * 0.5;
-    //                 point -= center;
-
-    //                 // The signal is only non-zero in a limited range, so don't waste samples
-    //                 let low = point.y - halfSize.y;
-    //                 let high = point.y + halfSize.y;
-    //                 let start = clamp(-3.0 * sigma, low, high);
-    //                 let end = clamp(3.0 * sigma, low, high);
-
-    //                 // Accumulate samples (we can get away with surprisingly few samples)
-    //                 let step = (end - start) / 4.0;
-    //                 let y = start + step * 0.5;
-    //                 let value = 0.0;
-
-    //                 value += roundedBoxShadowX(point.x, point.y - y, sigma, corner, halfSize) * gaussian(y, sigma) * step;
-    //                 y += step;
-
-    //                 value += roundedBoxShadowX(point.x, point.y - y, sigma, corner, halfSize) * gaussian(y, sigma) * step;
-    //                 y += step;
-
-    //                 value += roundedBoxShadowX(point.x, point.y - y, sigma, corner, halfSize) * gaussian(y, sigma) * step;
-    //                 y += step;
-
-    //                 value += roundedBoxShadowX(point.x, point.y - y, sigma, corner, halfSize) * gaussian(y, sigma) * step;
-    //                 y += step;
-
-    //                 return value;
-    //             }
-
-
-    //             fn pixel(self) -> vec4 {
-    //                 // let sdf = Sdf2d::viewport(self.pos * self.rect_size);
-    //                 // sdf.box(
-    //                 //     1,
-    //                 //     1,
-    //                 //     self.rect_size.x - 2.0,
-    //                 //     self.rect_size.y - 2.0,
-    //                 //     max(1.0, self.radius)
-    //                 // )
-    //                 let color = #0009;
-    //                 color.a *= roundedBoxShadow(vec2(0.0), self.rect_size * 0.5manolo01
-    //                 , self.pos * self.rect_size * 0.5, 1.0, self.radius * 1.2);
-    //                 // sdf.fill_keep(color);
-    //                 // return sdf.result;
-    //                 return color;
-    //             }
-
-    //             // fn vertex(self) -> vec4 {
-    //             //     return self.clip_and_transform_vertex(self.rect_pos, self.rect_size + 0.1);
-    //             // }
-
-    //             fn vertex(self) -> vec4 {
-    //                 let clipped: vec2 = clamp(
-    //                     self.geom_pos * vec2(self.rect_size.x + 320., self.rect_size.y + 320.) + self.rect_pos - vec2(160., 160.),
-    //                     self.draw_clip.xy,
-    //                     self.draw_clip.zw
-    //                 );
-    //                 self.pos = (clipped - self.rect_pos) / self.rect_size;
-    //                 return self.camera_projection * (self.camera_view * (
-    //                     self.view_transform * vec4(clipped.x, clipped.y, self.draw_depth + self.draw_zbias, 1.)
-    //                 ));
-    //             }
-    //         },
-
-    //         image = <Image> {
-    //             source: (IMG_0),
-    //             width: 60.0,
-    //             height: 60.0,
-
-    //             draw_bg: {
-    //                 instance radius: 10.0
-    //                 fn pixel(self) -> vec4 {
-    //                     let sdf = Sdf2d::viewport(self.pos * self.rect_size);
-    //                     sdf.box(
-    //                         1,
-    //                         1,
-    //                         self.rect_size.x - 2.0,
-    //                         self.rect_size.y - 2.0,
-    //                         max(1.0, self.radius)
-    //                     )
-    //                     sdf.fill_keep(self.get_color())
-    //                     //return #0000;
-    //                     return sdf.result;
-    //                 }
-    //             }
-    //         }
-    //     }
-
-    //     animator: {
-    //         play = {
-    //             default: init,
-    //             show = {
-    //                 redraw: true,
-    //                 from: {all: BounceLoop {duration: 1.0, end: 1.0}}
-    //                 apply: {
-    //                     image = { draw_bg: { radius: 15.0 } }
-    //                 }
-    //             }
-    //             init = {
-    //                 from: {all: Snap}
-    //                 apply: {
-    //                     element = { image = {draw_bg: { radius: 0.0 }} }
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
 
     Cell = <View> {
         width: Fit,
